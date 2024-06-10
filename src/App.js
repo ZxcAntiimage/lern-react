9a452ee5c57e92994e916586f9b4e4edad12f9ee
max <fasionergg@gmail.com>
2024-06-10 15:20:18 +0300

    lern react js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Header from './components/Header';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component{
+  text = "Hello world";
+  render(){
+    return(
+      <>
+     <Header title="Шапочка сайта"/>
+      
+      <div className="name">
+        <h1>{this.text}</h1>
+        <input placeholder='Hi' onClick={this.inputMouse} onMouseEnter={this.mouseOver} />
+        <p>{this.text === "Hello World" ? "Hi" : "Good bye"}</p>
+  
+      </div>
+   
+      </>
+    )
+  }
+
+  inputClick = () => {console.log("Input Clicked")}
+  mouseOver = () => {console.log("Mouse Over")}
 }
+
 
 export default App;
